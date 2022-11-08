dcfd27991ad030139c1914f39d6fdd245de6ac1c
wepa <wepa.abdysetdarov@gmail.com>
2022-11-08 16:42:11 +0500

    installed react-icons, created btn in Card.jsx and applied some CSS

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,15 @@
 import CardTools from "components/CardTools"
 import CardInfo from "components/CardInfo"
+import { FaTimes } from "react-icons/fa"
 
 function Card({item}) {
   return (
     <div className = "card">
       <CardInfo item = {item}/>
       <CardTools item = {item}/>
+      <button className="delete">
+        <FaTimes color="red"/>
+      </button>
     </div>
   )
 }
