746cf3500ebf8eb723b7a1e12b6db0383e6f759c
wepa <wepa.abdysetdarov@gmail.com>
2022-11-09 11:16:25 +0500

    created edit button in Card.jsx and applied some css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,30 @@
 import CardTools from "components/CardTools"
 import CardInfo from "components/CardInfo"
 import { FaTimes } from "react-icons/fa"
+import { FaEdit } from "react-icons/fa"
 
 function Card({item, handleDelete}) {
   const {id} = item
 
-  const handleClick = ()=>{
+  const onClickDelete = ()=>{
     if(window.confirm("Delete the Job?")){
       handleDelete(id)
     }
+  }
+
+  const onClickEdit = () => {
+
   }
   
   return (
     <div className = "card">
       <CardInfo item = {item}/>
       <CardTools item = {item}/>
-      <button className="delete" onClick={handleClick}>
+      <button className="delete" onClick={onClickDelete}>
         <FaTimes color="red"/>
+      </button>
+      <button className="edit" onClick={onClickEdit}>
+        <FaEdit color="grey"/>
       </button>
     </div>
   )
