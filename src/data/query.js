94859a46f0c30917dd0be8f9da27b2b2ff23c5de
wepa <wepa.abdysetdarov@gmail.com>
2022-11-09 14:33:58 +0500

    created new query in query.js to update database

--- src/data/query.js
+++ src/data/query.js
@@ -61,3 +61,38 @@ export const deleteJobsOne = (id) => `
     }
   }
   `
+export const updateJobsOne = (id, obj) => {
+  const {company, logo, newX, featured, position, role, level, contract, location, languages, tools} = obj
+
+  return `
+  mutation{
+    update_Jobs_by_pk(pk_columns: {id: ${id}}, _set: {
+      company: "${company}",
+      logo: "${logo}",
+      new: "${newX}",
+      featured: "${featured}",
+      position: "${position}",
+      role: "${role}",
+      level: "${level}",
+      contract: "${contract}",
+      location: "${location}",
+      languages: "${languages}",
+      tools: "${tools}"
+    }) {
+      id
+      company
+      logo
+      new
+      featured
+      position
+      role
+      level
+      created_at
+      contract
+      location
+      languages
+      tools
+    }
+  }
+  `
+}
