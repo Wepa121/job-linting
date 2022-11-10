b88e9a0c2ace1332c0d6f9df58c7e597f673612a
wepa <wepa.abdysetdarov@gmail.com>
2022-11-10 17:35:00 +0500

    mounted new comp on Card list and applied some conditions

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router-dom"
 import Card from "components/Card"
-
+import JobFilter from "./JobFilter"
 
 function CardList({list, handleDelete, handleEdit, filterObj, setFilter}) {
   const navigate = useNavigate()
+  const {role, level, languages, tools} = filterObj
+
   return (
     <div>
+      {(role || level || languages.length > 0 || tools.length > 0 ) && <JobFilter filterObj = {filterObj} setFilter = {setFilter}/>}
       <div>
         {list.map((item, index) => <Card item = {item} key = {index} 
                                     handleDelete = {handleDelete} 
