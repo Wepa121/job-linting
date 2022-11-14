23beac4f17be6469a28ea58651d5c319400e52e9
wepa <wepa.abdysetdarov@gmail.com>
2022-11-14 18:14:35 +0500

    used Yup validation schema in JobForm

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import { Formik } from "formik"
+import * as Yup from "yup"
 
 
 import Ara from "./Ara"
@@ -7,31 +8,29 @@ import Ara from "./Ara"
 function JobForm({handleAdd, edit, handleUpdate}) {
   const navigate = useNavigate() 
 
-  const handleError = (values) => {
-    const errors = {}
-    if(!values.company) errors.company = "Please insert Copmany name"
-    else if(values.company.trim().length < 5) errors.company = "Company name must be at least ten character"
-
-    if(!values.logo) errors.logo = "Please insert Logo name"
-    else if(values.logo.trim().length < 5) errors.logo = "Logo name must be at least ten character"
-
-    if(!values.location) errors.location = "Please insert location name"
-    else if(values.location.trim().length < 5) errors.location = "Location name must be at least ten character"
-
-    if(!values.position) errors.position = "Please insert Position name"
-    else if(values.position.trim().length < 5) errors.position = "Position name must be at least ten character"
-
-    if(!values.newX) errors.newX = "Please choose one of them (new)"
-
-    if(!values.featured) errors.featured = "Please choose one of them (featured)"
-
-    if(!values.role) errors.role = "Please choose one of them (role)"
-
-    if(!values.level) errors.level = "Please choose one of them (level)"
-
-    if(!values.contract) errors.contract = "Please choose one of them (contract)"
-    return errors
-  }
+  const JobsSchema = Yup.object().shape({
+    company : Yup.string()
+      .min(2, "Too short!")
+      .max(50, "Too Long!")
+      .required("Please insert Company Name"),
+    logo : Yup.string()
+      .min(2, "Too short!")
+      .max(50, "Too Long!")
+      .required("Please insert logo"),
+    location : Yup.string()
+      .min(2, "Too short!")
+      .max(50, "Too Long!")
+      .required("Please insert Location"),
+    position : Yup.string()
+      .min(2, "Too short!")
+      .max(50, "Too Long!")
+      .required("Please insert Position"),
+    newX: Yup.string().required("Please choose (new)"),
+    featured: Yup.string().required("Please choose (featured)"),
+    role: Yup.string().required("Please choose (role)"),
+    level: Yup.string().required("Please choose (level)"),
+    contract: Yup.string().required("Please choose (contract)")
+  })
   
   const handleSubmit = (values) => {
     if(edit && edit.isEdit){
@@ -59,7 +58,7 @@ function JobForm({handleAdd, edit, handleUpdate}) {
       languages: [],
       tools: []
     }}
-    validate = { handleError }
+    validationSchema = { JobsSchema }
     onSubmit = { handleSubmit }
     >
       {(formik) => {
